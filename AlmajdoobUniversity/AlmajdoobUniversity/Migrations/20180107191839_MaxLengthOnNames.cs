85b3cde673e1769d344609141a47f05124765b6e
AHMED-home <ahmedhome@almajdoob.com>
2018-01-09 23:14:12 -0500

    Added Almajdoob University

---
+++ AlmajdoobUniversity/AlmajdoobUniversity/Migrations/20180107191839_MaxLengthOnNames.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AlmajdoobUniversity.Migrations
+{
+    public partial class MaxLengthOnNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Student",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstMidName",
+                table: "Student",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Student",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstMidName",
+                table: "Student",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
---
+++ AlmajdoobUniversity/AlmajdoobUniversity/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlmajdoobUniversity.Models
+{
+    public class Student :Person
+    {
+        public Student () { }
+
+       
+        
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+            
+        
+        [Display(Name ="Email Address")]
+        [EmailAddress(ErrorMessage ="Invalid Email Address")]
+        public string Email { get; set; }
+
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
